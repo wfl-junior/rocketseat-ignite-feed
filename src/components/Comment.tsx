5a6ba10ec8430edf2eb5f2452c65e1d0f9c0a82a
Wallace Júnior <wflj1997@hotmail.com>
2022-07-01 22:33:55 -0300

    Realizando likes nos comentários

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,4 +1,5 @@
 import { ThumbsUp, Trash } from "phosphor-react";
+import { useState } from "react";
 import { CommentData } from "../types";
 import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
@@ -11,42 +12,54 @@ export const Comment: React.FC<CommentProps> = ({
   id,
   content,
   onDeleteComment,
-}) => (
-  <div className={styles.comment}>
-    <Avatar src="https://github.com/wfl-junior.png" hasBorder={false} />
+}) => {
+  const [likeCount, setLikeCount] = useState(0);
 
-    <div className={styles.commentBox}>
-      <div className={styles.commentContent}>
-        <header>
-          <div className={styles.authorAndTime}>
-            <strong>Wallace Júnior</strong>
+  function handleDeleteComment() {
+    onDeleteComment(id);
+  }
 
-            <time
-              title="1 de julho de 2022 às 16:45"
-              dateTime="2022-07-01 16:45:00"
+  function handleLikeComment() {
+    setLikeCount(likeCount => likeCount + 1);
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar src="https://github.com/wfl-junior.png" hasBorder={false} />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Wallace Júnior</strong>
+
+              <time
+                title="1 de julho de 2022 às 16:45"
+                dateTime="2022-07-01 16:45:00"
+              >
+                Cerca de 2h atrás
+              </time>
+            </div>
+
+            <button
+              type="button"
+              title="Deletar comentário"
+              onClick={handleDeleteComment}
             >
-              Cerca de 2h atrás
-            </time>
-          </div>
+              <Trash size={24} />
+            </button>
+          </header>
 
-          <button
-            type="button"
-            title="Deletar comentário"
-            onClick={() => onDeleteComment(id)}
-          >
-            <Trash size={24} />
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp weight="bold" />
+            Aplaudir <span>{likeCount}</span>
           </button>
-        </header>
-
-        <p>{content}</p>
+        </footer>
       </div>
-
-      <footer>
-        <button>
-          <ThumbsUp weight="bold" />
-          Aplaudir <span>20</span>
-        </button>
-      </footer>
     </div>
-  </div>
-);
+  );
+};
