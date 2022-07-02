7d1093b18f617903058e2e65370700f198039940
Wallace Júnior <wflj1997@hotmail.com>
2022-07-01 23:54:17 -0300

    modal para confirmação de deletar comentário

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,5 +1,5 @@
 import { ThumbsUp, Trash } from "phosphor-react";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { CommentData } from "../types";
 import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
@@ -14,6 +14,17 @@ export const Comment: React.FC<CommentProps> = ({
   onDeleteComment,
 }) => {
   const [likeCount, setLikeCount] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  function handleOpenModal() {
+    document.body.style.overflow = "hidden";
+    setIsModalOpen(true);
+  }
+
+  function handleCloseModal() {
+    document.body.style.overflow = "";
+    setIsModalOpen(false);
+  }
 
   function handleDeleteComment() {
     onDeleteComment(id);
@@ -24,42 +35,71 @@ export const Comment: React.FC<CommentProps> = ({
   }
 
   return (
-    <div className={styles.comment}>
-      <Avatar src="https://github.com/wfl-junior.png" hasBorder={false} />
+    <Fragment>
+      {isModalOpen && (
+        <div className={styles.modalWrapper}>
+          <div className={styles.modal}>
+            <strong>Excluir comentário</strong>
+            <p>Você tem certeza que gostaria de excluir este comentário?</p>
 
-      <div className={styles.commentBox}>
-        <div className={styles.commentContent}>
-          <header>
-            <div className={styles.authorAndTime}>
-              <strong>Wallace Júnior</strong>
-
-              <time
-                title="1 de julho de 2022 às 16:45"
-                dateTime="2022-07-01 16:45:00"
+            <div>
+              <button
+                type="button"
+                onClick={handleCloseModal}
+                className={styles.cancelButton}
               >
-                Cerca de 2h atrás
-              </time>
+                Cancelar
+              </button>
+
+              <button
+                type="button"
+                onClick={handleDeleteComment}
+                className={styles.deleteButton}
+              >
+                Sim, excluir
+              </button>
             </div>
-
-            <button
-              type="button"
-              title="Deletar comentário"
-              onClick={handleDeleteComment}
-            >
-              <Trash size={24} />
-            </button>
-          </header>
-
-          <p>{content}</p>
+          </div>
         </div>
+      )}
 
-        <footer>
-          <button onClick={handleLikeComment}>
-            <ThumbsUp weight="bold" />
-            Aplaudir <span>{likeCount}</span>
-          </button>
-        </footer>
+      <div className={styles.comment}>
+        <Avatar src="https://github.com/wfl-junior.png" hasBorder={false} />
+
+        <div className={styles.commentBox}>
+          <div className={styles.commentContent}>
+            <header>
+              <div className={styles.authorAndTime}>
+                <strong>Wallace Júnior</strong>
+
+                <time
+                  title="1 de julho de 2022 às 16:45"
+                  dateTime="2022-07-01 16:45:00"
+                >
+                  Cerca de 2h atrás
+                </time>
+              </div>
+
+              <button
+                type="button"
+                title="Deletar comentário"
+                onClick={handleOpenModal}
+              >
+                <Trash size={24} />
+              </button>
+            </header>
+
+            <p>{content}</p>
+          </div>
+
+          <footer>
+            <button type="button" onClick={handleLikeComment}>
+              <ThumbsUp weight="bold" />
+              Aplaudir <span>{likeCount}</span>
+            </button>
+          </footer>
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
